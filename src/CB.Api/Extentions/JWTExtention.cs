0aeb991bfc5175215e9a75c3d964ea2a0535e271
Hồ Văn Toàn <it.vantoan@gmail.com>
2024-08-28 23:59:36 +0700

    Fix: Add auth with google

--- src/CB.Api/Extentions/JWTExtention.cs
+++ src/CB.Api/Extentions/JWTExtention.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.Google;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.IdentityModel.Tokens;
@@ -7,18 +8,34 @@ namespace CB.Api.Extentions;
 
 public static class JWTExtention {
 
-    public static IServiceCollection AddJWT(this IServiceCollection services, IConfiguration configuration) {
-        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-              .AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters {
-                  ValidateIssuer = false,
-                  ValidateAudience = false,
-                  ValidateLifetime = true,
-                  ValidateIssuerSigningKey = true,
-                  RequireExpirationTime = false,
-                  IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtSecret"]!))
-              });
+    public static IServiceCollection AddAuth(this IServiceCollection services, IConfiguration configuration) {
+        services.AddAuthentication(options => {
+            options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
+        }).AddJwtBearer(GoogleDefaults.AuthenticationScheme, options => {
+            options.Audience = configuration["Authentication:Google:ClientId"];
+            options.Authority = "https://accounts.google.com";
+            options.TokenValidationParameters = new TokenValidationParameters {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true
+            };
+        }).AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            RequireExpirationTime = false,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtSecret"]!))
+        });
 
-        _ = services.AddAuthorization(options => options.DefaultPolicy = new AuthorizationPolicyBuilder(JwtBearerDefaults.AuthenticationScheme).RequireAuthenticatedUser().Build());
+        _ = services.AddAuthorization(options => {
+            options.DefaultPolicy = new AuthorizationPolicyBuilder()
+                .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme, GoogleDefaults.AuthenticationScheme)
+                .RequireAuthenticatedUser()
+                .Build();
+        });
 
         return services;
     }
